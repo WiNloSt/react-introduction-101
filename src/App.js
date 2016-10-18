65585855c537b2171dfae08dbf973b7c06f1a54e
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-10-18 22:32:49 +0700

    Support SCSS files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Editor from './components/Editor/index.js'
 
 class App extends Component {
   render() {
     return (
       <div>
-        ready to hack!!
+        <Editor />
       </div>
     )
   }
---
+++ src/components/Editor/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import style from './style.scss'
+
+export default () => (
+  <div className={style.test}>Code editor</div>
+)
