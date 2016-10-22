ff3ec192bf1724575f69419ae58b3e68978c8c55
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-10-23 00:17:57 +0700

    Shamelessly copy stringify from jsbin

--- src/handleWorker.js
+++ src/handleWorker.js
@@ -1,9 +1,19 @@
 import worker from './webWorker'
 import { appendConsoleOutput } from './actions'
+import stringify from './lib/stringify'
+
+window.stringify = stringify
 
 function configureWorker(store) {
   worker.onmessage = message => {
-    const { data } = message
+    let { data } = message
+    data = data.map(arg => {
+      if (typeof arg === 'undefined') {
+        return 'undefined'
+      }
+
+      return stringify(arg)
+    })
     store.dispatch(appendConsoleOutput(data))
   }
 }
