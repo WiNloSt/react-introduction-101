ed154f9f60e35bbb598d64ef5d6a5231a446e276
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-10-23 14:32:01 +0700

    Add run button

--- src/components/Console/index.js
+++ src/components/Console/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import AcceptStyleAndClassName from '../../containers/AcceptStyleAndClassName'
-import { clearConsole } from '../../actions'
+import { clearConsole, runCode } from '../../actions'
 import style from './style.scss'
 
-const Console = ({ consoleOutput, clearConsole }) => (
+const Console = ({ consoleOutput, clearConsole, runCode }) => (
   <div className={style.console}>
     <header className={style.header}>
       <h1 className={style.headerName}>Console</h1>
-      <button className={style.clearLogButton} onClick={clearConsole}>Clear log</button>
+      <div className={style.consoleControl}>
+        <button className={style.button} onClick={runCode}>Run</button>
+        <button className={style.button} onClick={clearConsole}>Clear log</button>
+      </div>
     </header>
     <ul className={style.outputList}>
       {
@@ -34,7 +37,8 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    clearConsole: () => dispatch(clearConsole())
+    clearConsole: () => dispatch(clearConsole()),
+    runCode: () => dispatch(runCode())
   }
 }
 
