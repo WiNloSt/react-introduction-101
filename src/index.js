a9bf6ad2caaae4ed387b03be9d585621b7da4aea
Mahatthana Nomsawadi <mahatthana.n@gmail.com>
2016-10-23 11:34:39 +0700

    Preserve redux state when hot loading
    Use react-hot-loader v3 beta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'rxjs' // required to use with redux-observable
+import { AppContainer } from 'react-hot-loader'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -11,13 +12,33 @@ import configureWorker from './handleWorker'
 const store = configureStore()
 configureWorker(store)
 
+const rootElement = document.getElementById('root')
 const enhancedApp = (
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <AppContainer>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AppContainer>
 )
 
 ReactDOM.render(
   enhancedApp,
-  document.getElementById('root')
+  rootElement
 )
+
+if (module.hot) {
+  module.hot.accept('./App.js', () => {
+    const NextApp = require('./App').default
+    const enhancedApp = (
+      <AppContainer>
+        <Provider store={store}>
+          <NextApp />
+        </Provider>
+      </AppContainer>
+    )
+    ReactDOM.render(
+    enhancedApp,
+      rootElement
+    )
+  })
+}
